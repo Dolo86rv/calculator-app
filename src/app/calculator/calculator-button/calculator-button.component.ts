873101fae0f15039f765b5645b57d58051717d3e
Dolorv86 <dolorv86@gmail.com>
2025-08-18 14:03:10 +0200

    Agregando funcionalidad a la calculadora

--- src/app/calculator/calculator-button/calculator-button.component.ts
+++ src/app/calculator/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, input, output, signal, viewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -8,10 +8,18 @@ import { ChangeDetectionStrategy, Component, HostBinding, input, ViewEncapsulati
   styleUrls: ['./calculator-button.component.css'],
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()'
   },
   //encapsulation: ViewEncapsulation.None,
 })
 export class CalculatorButtonComponent{
+
+  //Eimitir eventos de click al padre
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+  public isPressed = signal(false);
+
   public isCommand = input(false, {
     transform: ( value: boolean | string) =>
       typeof value === 'string' ? value === '' : value
@@ -26,8 +34,32 @@ export class CalculatorButtonComponent{
   //  return this.isCommand() ? true : false;
   //}
 
-  @HostBinding('class.w-2/4') get doubleSizeStyle() {
-    return this.isDoubleSize() ? true : false;
+  //@HostBinding('class.w-2/4') get doubleSizeStyle() {
+  //  return this.isDoubleSize() ? true : false;
+  //}
+
+  handleClick() {
+    if (!this.contentValue()?.nativeElement) {
+      return;
+    }
+    const value = this.contentValue()!.nativeElement.innerText;
+    this.onClick.emit(value.trim());
+  }
+
+  public keyboardPressedStyle(key: string) {
+    if(!this.contentValue()?.nativeElement) return;
+
+    const value = this.contentValue()!.nativeElement.innerText.trim();
+
+    if ( value !== key) return;
+
+    this.isPressed.set(true);
+
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 100);
+
+
   }
 
   //ngOnInit(): void {
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,15 +1,52 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, viewChildren} from '@angular/core';
 import { CalculatorButtonComponent } from "@/calculator/calculator-button/calculator-button.component";
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
   imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  }
   //styles: `
   //  .is-command {
   //    @apply bg-indigo-700 bg-opacity-20;
   //  }
   //`
 })
-export class CalculatorComponent { }
+export class CalculatorComponent {
+
+  private calculatorService = inject(CalculatorService);
+
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+  handleClick(key: string) {
+    this.calculatorService.constructNumber(key);
+  }
+
+  public resultText = computed(() => this.calculatorService.resultText());
+  public subResultText = computed(() => this.calculatorService.subResultText());
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
+
+  //@HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const key = event.key;
+
+    const keyEquivalents: Record<string, string> = {
+      'Escape': 'C',
+      'Backspace': 'C',
+      'Enter': '=',
+      'Delete': 'C',
+      '*': 'x',
+      '/': '÷',
+    }
+    const keyValue = keyEquivalents[key] ?? key;
+
+    this.handleClick(keyValue);
+
+    this.calculatorButtons().forEach( button => {
+      button.keyboardPressedStyle(keyValue);
+    });
+  }
+}
